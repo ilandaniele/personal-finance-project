c0033c31b5045ee83cf25b620c43a42a89a8ac5c
MatiasJRB <mjrcal95@hotmail.com.ar>
2021-03-14 17:07:48 -0300

    changes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,49 +1,141 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
+  <div class="q-pa-xs ">
+    <q-layout c view="lHh Lpr lff" class="shadow-2 rounded-borders " >
+      <q-header elevated class="primary">
+        <q-toolbar>
+          <q-toolbar-title>Inicio</q-toolbar-title>
+          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+        </q-toolbar>
+      </q-header>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+      <q-drawer
+        v-model="drawer"
+        show-if-above
+        :width="180"
+        :breakpoint="500"
+      >
+        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="home" />
+              </q-item-section>
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+              <q-item-section>
+                Inicio
+              </q-item-section>
+            </q-item>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+            <!--q-item active clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="add" />
+              </q-item-section>
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+              <q-item-section>
+                Nuevo movimiento
+              </q-item-section>
+            </q-item-->
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+
+              <q-item-section>
+                Movimientos
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="settings" />
+              </q-item-section>
+
+              <q-item-section>
+                Configuración
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="help" />
+              </q-item-section>
+
+              <q-item-section>
+                Ayuda
+              </q-item-section>
+            </q-item>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="info" />
+              </q-item-section>
+
+              <q-item-section>
+                Acerca de
+              </q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="logout" />
+              </q-item-section>
+
+              <q-item-section>
+                Salir
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <div class="text-weight-bold" >Matias Rios</div>
+            <div>@matiasrios</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+      <q-page-container>
+        <q-page padding class="q-mt-xl window-width row justify-center ">
+          <q-page >
+              <div class="text-h2 text-weight-thin " style="opacity:.9">
+                Saldo:  ${{saldo}}
+              </div>
+          </q-page>
+
+          <q-page >
+              <div class="text-h2 " >
+                {{saldo}}
+              </div>
+          </q-page>              
+        </q-page>
+        <q-page padding class="q-pa-xl">
+            <template>
+            <div class="q-pa-md">
+              <q-table
+                title="Treats"
+                :data="data"
+                :columns="columns"
+                row-key="name"
+              />
+            </div>
+          </template>
+        </q-page>
+        
+        <q-page-sticky position="bottom-right" :offset="[18, 18]">
+            <q-btn fab icon="add" color="primary"  />
+          </q-page-sticky>
+        
+          
+      </q-page-container>
+      
+    </q-layout>
+    
+  </div>
+  
 </template>
 
 <script>
@@ -99,9 +191,68 @@ export default {
   components: { EssentialLink },
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+      data: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%'
+        }],
+      columns: [
+        {
+          name: 'name',
+          required: true,
+          label: 'Dessert (100g serving)',
+          align: 'left',
+          field: row => row.name,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'calories',
+          align: 'center',
+          label: 'Calories',
+          field: 'calories',
+          sortable: true
+        },
+        { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true }
+      ],
+      saldo: 777.7,
+      nombre_user: "Matias Rios",
+      drawer: false
     }
+  },
+  created()
+  {
   }
 }
+
+
 </script>
+
+<style lang="sass">
+.my-sticky-header-table
+  /* height or max-height is important */
+  height: 310px
+
+  .q-table__top,
+  .q-table__bottom,
+  thead tr:first-child th
+    /* bg color is important for th; just specify one */
+    background-color: #c1f4cd
+
+  thead tr th
+    position: sticky
+    z-index: 1
+  thead tr:first-child th
+    top: 0
+
+  /* this is when the loading indicator appears */
+  &.q-table--loading thead tr:last-child th
+    /* height of all previous header rows */
+    top: 48px
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+    <div class="q-pa-md">
+              <q-table
+                title="Treats"
+                :data="data"
+                :columns="columns"
+                row-key="name"
+              />
+            </div>
+  </q-page>
+  
+
+  
+</template>
+
+<script>
+export default {
+  data:[],
+  columns: [],
+  name: 'PageIndex'
+}
+</script>
